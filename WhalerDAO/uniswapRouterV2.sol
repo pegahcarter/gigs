64fe4944e9847fa83c014fdd32e8ac078d6acd72
Carl Farterson <carlfarterson@gmail.com>
2020-12-16 16:05:56 -0600

    Add functionality for max pledge/unpledge

--- WhalerDAO/uniswapRouterV2.sol
+++ WhalerDAO/uniswapRouterV2.sol
@@ -20,22 +20,21 @@ interface IUniswapRouterv2Router01 {
     function swapExactTokensForTokens(uint256 amountIn,uint256 amountOutMin,address[] calldata path,address to,uint256 deadline) external returns (uint[] memory amounts);
 }
 
-contract Reserve2 {
+contract uniswapRouterV2 {
     using SafeMath for uint256;
 
     event Pledge(uint256 _numCampaign, address _addr, uint256 _amount);
     event Unpledge(uint256 _numCampaign, address _addr, uint256 _amount);
     event Rebase(uint256 _id);
-    event ReserveTransfer(address _token, address _to, uint256 _amount);
+    event WithdrawToken(address _token, address _to, uint256 _amount);
 
-    address constant public DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address constant public TREE = 0xCE222993A7E4818E0D12BC56376c5a60f92A5783;
-    address constant public RESERVE = 0x390a8Fb3fCFF0bB0fCf1F91c7E36db9c53165d17;
+    address constant private TREE = 0xCE222993A7E4818E0D12BC56376c5a60f92A5783;
+    address constant private RESERVE = 0x390a8Fb3fCFF0bB0fCf1F91c7E36db9c53165d17;
+    address constant private DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
 
-    IUniswapV2Router01 public router;
-    address public tree;
     address public gov;
-    address public charity;
+    IERC20 public tree = IERC20(TREE);
+    IERC20 public reserveToken = IERC20(DAI);
 
     struct Campaign {
         uint256 totalPledged;
@@ -51,56 +50,14 @@ contract Reserve2 {
  
     constructor(address _gov) public {
         gov = _gov;
-        router = IUniswapV2Router01(_router);
     }
 
-    
-    function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external override returns (uint256[] memory amounts) {
-        require(deadline >= block.timestamp, 'UniswapV2Router: EXPIRED');
-
-        Campaign storage c = campaigns[numCampaigns];
-        require(c.totalPledged >= amountIn, "Not enough DAI pledged. Rebase postponed.");
-
-        // transfer pledged DAI to reserve
-        IERC20(DAI).increaseAllowance(address(this), c.totalPledged);
-        IERC20(DAI).transfer(RESERVE, c.totalPledged);
-
-        // https://github.com/WhalerDAO/tree-contracts/blob/4525d20def8fce41985f0711e9b742a0f3c0d30b/contracts/TREEReserve.sol#L230
-        address tree = address(path[0]);
-    
-        // Send TREE to each pledger
-        for (uint i=1; i<c.numPledgers+1; i++) {
-            
-            address pledger = c.pledgers[i];
-            uint256 amountPledged = c.amountsPledged[pledger];
-
-            // treeToReceive = value pledged * (amountIn / totalPledged)
-            // For example, if 100 DAI is pledged and there's only 50 TREE available
-            // an address that pledged 5 DAI would receive 5 * (50/100) = 2.5 TREE
-            uint256 treeToReceive = amountPledged.mul(amountIn).div(c.totalPledged);
-
-            // Only transfer to EOAs to prevent unexpected reverts if pledge was done using CREATE2
-            // note: TREE is already approved to transfer
-            // https://github.com/WhalerDAO/tree-contracts/blob/4525d20def8fce41985f0711e9b742a0f3c0d30b/contracts/TREEReserve.sol#L228
-            if !Address.isContract(pledger) {
-                IERC20(TREE).transfer(pledger, treeToReceive);
-                c.treeSold = c.treeSold + treeToReceive;
-            }
-        }
-
-        numCampaigns++;
-
-        uint256[] memory amounts = new uint256[](2);
-        amounts[0] = amountIn;
-        amounts[1] = c.totalPledged;
-    }
-
-
-    function pledge(uint256 _amount) external payable returns (bool) {
+    function pledge(uint256 _amount, bool max) external payable returns (bool) {
         require(!Address.isContract(msg.sender), "Must pledge from EOA");
-        // Handle incoming DAI
-        require(IERC20(DAI).balanceOf(msg.sender) >= _amount, "Cannot pledge more DAI than held.")
-        IERC20(DAI).transferFrom(msg.sender, address(this), _amount);
+        if (max) {_amount = reserveToken.balanceOf(msg.sender);}
+        require(_amount > 0, "Must pledge more than 0.");
+        require(reserveToken.balanceOf(msg.sender) >= _amount, "Cannot pledge more reserveToken than held.");
+        reserveToken.transferFrom(msg.sender, address(this), _amount);
 
         Campaign storage c = campaigns[numCampaigns];
         c.totalPledged = c.totalPledged + _amount;
@@ -117,30 +74,64 @@ contract Reserve2 {
         }
 
         emit Pledge(numCampaigns, msg.sender, _amount);
-
     }
 
-    function unpledge(uint256 _amount) external payable {
+
+    function unpledge(uint256 _amount, bool max) external payable {
         Campaign storage c = campaigns[numCampaigns];
 
         uint256 pledgerId = getPledgerId(numCampaigns, msg.sender);
         require(pledgerId != 0, "User has not pledged.");
+        if (max) {_amount = c.amountsPledged[msg.sender];}
         require(_amount <= c.amountsPledged[msg.sender], "Cannot unpledge more than already pledged.");
 
         c.totalPledged = c.totalPledged.sub(_amount);
         c.amountsPledged[msg.sender] = c.amountsPledged.sub(_amount);
 
-        // TODO: handle unpledge token
+        reserveToken.transfer(msg.sender, _amount);
 
         emit Unpledge(numCampaigns, msg.sender, _amount);
     }
 
-    function reserveTransfer(address _token, address _to) external payable {
-        require(msg.sender == gov, "UniswapRouter: not gov");
-        amount = IERC20(_token).balanceOf(address(this));
-        IERC20(_token).transfer(_to, amount);
-        emit ReserveTransfer(_token, _to, amount);
+
+    function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external override returns (uint256[] memory amounts) {
+        require(deadline >= block.timestamp, 'UniswapV2Router: EXPIRED');
+
+        Campaign storage c = campaigns[numCampaigns];
+        require(c.totalPledged >= amountIn, "Not enough DAI pledged. Rebase postponed.");
+
+        // transfer pledged reserveToken to reserve
+        reserveToken.increaseAllowance(address(this), c.totalPledged);
+        reserveToken.transfer(RESERVE, c.totalPledged);
+
+        // Send TREE to each pledger
+        for (uint i=1; i<c.numPledgers+1; i++) {
+            
+            address pledger = c.pledgers[i];
+            uint256 amountPledged = c.amountsPledged[pledger];
+
+            // treeToReceive = value pledged * (amountIn / totalPledged)
+            // For example, if 100 DAI is pledged and there's only 50 TREE available
+            // an address that pledged 5 DAI would receive 5 * (50/100) = 2.5 TREE
+            uint256 treeToReceive = amountPledged.mul(amountIn).div(c.totalPledged);
+
+            // Only transfer to EOAs to prevent unexpected reverts if pledge was done using CREATE2
+            // Also, if user ended up unpledging 100%, do not waste a transfer of 0 tokens
+            // note: TREE is already approved to transfer
+            // https://github.com/WhalerDAO/tree-contracts/blob/4525d20def8fce41985f0711e9b742a0f3c0d30b/contracts/TREEReserve.sol#L228
+            if (!Address.isContract(pledger) && amountPledged > 0) {
+                tree.transfer(pledger, treeToReceive);
+                c.treeSold = c.treeSold + treeToReceive;
+            }
+        }
+        numCampaigns++;
+
+        // Return amounts based on https://github.com/WhalerDAO/tree-contracts/blob/4525d20def8fce41985f0711e9b742a0f3c0d30b/contracts/TREEReserve.sol#L217
+        uint256[] memory amounts = new uint256[](2);
+        amounts[0] = amountIn;
+        amounts[1] = c.totalPledged;
     }
+
 
     function getPledgerId(uint256 _numCampaign, address _addr) private returns (uint256 pledgerId) {
         Campaign storage c = campaigns[_numCampaign];
@@ -154,6 +145,20 @@ contract Reserve2 {
         }
     }
 
+
+    function setReserveToken(address _newToken) external {
+        require(msg.sender == gov, "UniswapRouter: not gov");
+        reserveToken = IERC20(_newToken);
+    }
+
+
+    function withdrawToken(address _token, address _to, uint256 _amount, bool max) external payable {
+        require(msg.sender == gov, "UniswapRouter: not gov");
+        if (max) {_amount = IERC20(_token).balanceOf(address(this));}
+        IERC20(_token).transfer(_to, _amount);
+        emit WithdrawToken(_token, _to, _amount);
+    }
+
     function getCampaignTotalPledged(uint256 _numCampaign) public view returns (uint256) {
         Campaign storage c = campaigns[_numCampaign];
         return c.totalPledged;
@@ -163,7 +168,7 @@ contract Reserve2 {
         Campaign storage c = campaigns[_numCampaign];
         pledged = false;
         for (i=1;i<c.numPledgers;i++) {
-            if (c.pledgers[i] == _addr) {
+            if (c.pledgers[i] == _addr && c.amountsPledged[_addr] > 0) {
                 pledged = true;
                 break;
             }
